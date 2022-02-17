2ba0b7988a6e1b9ca46d1a58beb331d8d95f461c
Aarya <aarya@Aaryas-MacBook-Air.local>
2022-02-17 09:32:42 -0800

    updated card, pile class

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,91 @@
+import java.util.HashMap;
 
 /** represents a playing card that can draw itself. */
 public class Card implements Drawable, Updateable{
+    public class Card implements Comparable<Card>{
+        // need some instance variables
+        public static final HashMap<Integer, String> dictValues = new HashMap<Integer, String>();
+        private int suit;
+        private int value;
+        //1 is Spades
+        //2 is Clubs
+        //3 is Hearts
+        //4 is Diamonds
+        static{
+            dictValues.put(1,"Ace");
+            dictValues.put(2,"2");
+            dictValues.put(3,"3");
+            dictValues.put(4,"4");
+            dictValues.put(5,"5");
+            dictValues.put(6,"6");
+            dictValues.put(7,"7");
+            dictValues.put(8,"8");
+            dictValues.put(9,"9");
+            dictValues.put(10,"10");
+            dictValues.put(11,"Jack");
+            dictValues.put(12,"Queen");
+            dictValues.put(13,"King");
+        }
+        /** Must have this constructor.  You can add others*/
+        public Card(int suit, int value){
+            this.suit=suit;
+            this.value=value;
+
+        }
+
+        //getters
+        public int getSuit(){
+            return suit;
+        }
+        public int getValue(){
+            return value;
+        }
+
+
+        @Override
+        /** This method satisfies the Comparable interface which determines
+         * if this Object is smaller than, greater than or equal to the
+         * specified Card c
+         * Formally, if this Card is smaller than c, a negative int is returned
+         *           if this Card is larger than c, a positive int is returned
+         *           if this Card is equal to c, zero is returned	*/
+        public int compareTo(Card c){
+            int answer = 0;
+            if (value<c.getValue()){
+                answer=-1;
+            }
+            else if (value>c.getValue()){
+                answer=1;
+            }
+            else {
+                answer=0;
+            };
+            return answer;
+        }
+
+        // represents this Card in the following manner
+        // if the card is the Ace of Spades, then
+        // it returns "Ace of Spades". 2 - 10 can be represented
+        // as "2 of Hearts" or "Two of Hearts".  Your choice.
+        @Override
+        public String toString(){
+            String s = "";
+            s+=dictValues.get(value);
+            s+=" of ";
+            if (suit==1) {
+                s+="Spades";
+            }
+            else if (suit==2) {
+                s+="Clubs";
+            }
+            else if (suit==3) {
+                s+="Hearts";
+            }
+            else if (suit==4) {
+                s+="Diamonds";
+            }
+            return s;
+        }
+    }
 
 }
--- src/Pile.java
+++ src/Pile.java
@@ -1,12 +1,86 @@
+import java.util.*;
 
-/** A Pile is a collection of cards.  This needs to be
- * Drawable because it will be shown on the GUI. Put code
- * here that ALL Piles share.  The ways in which Piles are 
- * different belong in the subclasses.  The draw method should 
- * be implemented here.  Updateable may have empty implementation.
- * You WILL write subclasses of Pile
- */
-public abstract class Pile implements Drawable, Updateable {
-    
-    public abstract boolean canAddCard(Card c);
+public class Pile {
+
+    protected ArrayList<Card> list=new ArrayList<>();
+    // returns the number of Cards left in the Pile
+    public int size(){
+        return list.size();
+    }
+
+    public ArrayList<Card> getList() {
+        return list;
+    }
+    //clear pile//
+    public void clear(){
+        ArrayList<Card> placeholder=new ArrayList<Card>();
+        list=placeholder;
+    }
+
+    public void shuffle() {
+        /* Randomizes the cards in this deck.
+         * You must use the following algorithm:
+         * If the size is < 2, return;
+         * If the size == 2, "flip a coin", swap if "heads"
+         * If the size > 2 repeat the following steps size() * 2 times:
+         * select two different indexes (make sure they are different)
+         * swap them in the ArrayList
+         */
+        if (size()<2) return;
+        if (size()==2) {
+            double coin=Math.random();
+            if (coin>=0.5) {
+                Card x=list.get(0);
+                Card y=list.get(1);
+                list.set(0, y);
+                list.set(1,x);
+            }
+        }
+        if (size()>2) {
+            int indOne= (int)(Math.random() * (list.size()-1)) + 1;
+            int indTwo= (int)(Math.random() * (list.size()-1)) + 1;
+            Card x=list.get(indOne);
+            Card y=list.get(indTwo);
+            list.set(indOne, y);
+            list.set(indTwo,x);
+        }
+    }
+
+    /*
+     * Returns the "top" Card from this Pile, removing it from its
+     *  data structure */
+
+    public Card deal(){
+        Card c = list.remove(0);
+        return c;
+    }
+
+
+    /*  Adds every Card from p to this Pile
+        You should not have to change this method.
+    */
+    public void addPile(Pile p){
+        while(p.size()>0){
+            this.add(p.deal());
+        }
+    }
+
+
+    public void add(Card c) {
+        list.add(c);
+    }
+    /* This returns a String representation of this Pile.  It should return
+     *  a String built out of the Cards that the pile contains
+     */
+
+    @Override
+    public String toString(){
+        String s = "";
+        //iterate thru list, add to string
+        for (Card c: list) {
+            s+=c;
+            s+=",";
+        }
+        return s;
+    }
 }
